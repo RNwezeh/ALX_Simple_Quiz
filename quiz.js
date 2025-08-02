8e6474b77afd2c8439870b0b2cc6fa2d4ef0aa10
ROSEQUEEN NWEZEH <rosequeen.nwezeh@gmail.com>
2025-08-02 06:02:33 +0100

    js update

---
+++ quiz.js
@@ -0,0 +1,15 @@
+function checkAnswer(){
+    const correctAnswer="4";
+    const seletedOption = document.querySelector('input[name="quiz"]:checked');
+    const userAnswer = selectedOption ? selectedOption.value : null;
+    const feedbackElement = document.getElementById("feedback");
+
+    if (userAnswer===correctAnswer){
+        feedbackElement.textContent = "correct! Well done.";
+    } else{
+        feedbackElement.textContent = "That's incorrect. Try again!.";
+    }
+
+    document.getElementById("submit-answer").addEventListener("click", checkAnswer);
+
+}
